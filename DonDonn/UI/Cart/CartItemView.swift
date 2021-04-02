4bb4b284013e5bf2c750b0e21c8a4149db2a49cc
Shaban <sh3ban.kamel@gmail.com>
2021-04-02 14:56:19 +0200

    Encapsulate CartItemView in a separate class

---
+++ DonDonn/UI/Cart/CartItemView.swift
@@ -0,0 +1,40 @@
+//
+// Created by Apple on 02/04/2021.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct CartItemView: View {
+    var item: MenuItem
+
+    var body: some View {
+        HStack {
+            AnimatedImage(url: URL(string: item.image))
+                    .resizable()
+                    .frame(width: 60, height: 60)
+
+            Text(item.title)
+
+            Spacer()
+
+            Text(item.price)
+
+            Button(action: {
+                print("Round Action")
+            }) {
+                ZStack(alignment: .center) {
+                    Circle()
+                            .frame(width: 20, height: 20)
+                            .foregroundColor(Color(UIColor.gray.withAlphaComponent(0.1)))
+                            .shadow(radius: 10)
+
+                    Text("x")
+                }
+            }
+                    .padding(.leading, 5)
+        }.padding(.bottom)
+    }
+
+}
+
--- DonDonn/UI/Cart/CartView.swift
+++ DonDonn/UI/Cart/CartView.swift
@@ -5,7 +5,6 @@
 //
 
 import SwiftUI
-import SDWebImageSwiftUI
 
 struct CartView: View {
     var menuItems = [MenuItem]()
@@ -17,34 +16,6 @@ struct CartView: View {
             }
         }.background(Color(.white))
                 .padding()
-    }
-
-    private func CartItemView(item: MenuItem) -> some View {
-        HStack {
-            AnimatedImage(url: URL(string: item.image))
-                    .resizable()
-                    .frame(width: 60, height: 60)
-
-            Text(item.title)
-
-            Spacer()
-
-            Text(item.price)
-
-            Button(action: {
-                print("Round Action")
-            }) {
-                ZStack(alignment: .center) {
-                    Circle()
-                            .frame(width: 20, height: 20)
-                            .foregroundColor(Color(UIColor.gray.withAlphaComponent(0.1)))
-                            .shadow(radius: 10)
-
-                    Text("x")
-                }
-            }
-                    .padding(.leading, 5)
-        }.padding(.bottom)
     }
 
 }
