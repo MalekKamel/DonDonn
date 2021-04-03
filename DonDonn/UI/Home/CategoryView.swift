01aa80022acb9750dc7993c3ccc83a546968a715
Shaban <sh3ban.kamel@gmail.com>
2021-04-03 19:51:21 +0200

    Use SwiftGen for accessing assets and colors

--- DonDonn/UI/Home/CategoryView.swift
+++ DonDonn/UI/Home/CategoryView.swift
@@ -13,10 +13,10 @@ struct CategoryView: View {
             Text(item.name)
                     .font(.system(size: 22))
                     .fontWeight(isSelected ? .bold : .none)
-                    .foregroundColor(Color(isSelected ? "mainfont" : "subfont"))
+                    .foregroundColor(Color(isSelected ? Asset.Assets.mainfont.name : Asset.Assets.subfont.name))
 
             Capsule()
-                    .fill(Color("mainfont"))
+                    .fill(Color(Asset.Assets.mainfont.name))
                     .frame(width: 30, height: 4)
                     .opacity(isSelected ? 1 : 0)
         }
