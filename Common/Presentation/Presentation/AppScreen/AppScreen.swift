febae89a6253329310e31e963824b3fd911e68b9
Shaban <sh3ban.kamel@gmail.com>
2021-04-05 00:08:15 +0200

    Add documentaion to AppScreen

--- Common/Presentation/Presentation/AppScreen/AppScreen.swift
+++ Common/Presentation/Presentation/AppScreen/AppScreen.swift
@@ -4,15 +4,42 @@
 
 import SwiftUI
 
+/// A protocol that abstracts the common functionalities of a screen
+/// The screen represents the main container that covers all the device screen.
+/// NOT a single component.
 public protocol AppScreen: View {
     associatedtype Presenter: AppPresenter
+
+    /// The presenter of this screen
     var presenter: Presenter { get set }
 
+    /// The main container for the whole screen
+    /// - Returns: AnyView
     func BodyView() -> AnyView
+
+    /// The normal content view that should be shown to
+    /// the user in the normal state.
+    /// - Returns: AnyView
     func ContentView() -> AnyView
+
+    /// The screen initial view.
+    /// This view appears on loading the screen first time.
+    /// - Returns: AnyView
     func InitialView() -> AnyView
+
+    /// The screen loading view.
+    /// - Returns: AnyView
     func LoadingView() -> AnyView
+
+    /// The screen error view.
+    /// - Returns: AnyView
     func ErrorView(error: Swift.Error) -> AnyView
+
+    /// Called when the initial view appears.
+    /// By default, this function loads the content view by changing
+    /// the screen state to loaded.
+    /// Override this function to do call API for example before
+    /// showing the content view.
     func onInitialViewAppear()
 }
 
@@ -39,8 +66,9 @@ public extension AppScreen {
         Color.clear.eraseToAnyView()
     }
 
+
     func onInitialViewAppear() {
-        // Default implementation
+        // Show the content view by default
         presenter.screenState = .loaded
     }
 
